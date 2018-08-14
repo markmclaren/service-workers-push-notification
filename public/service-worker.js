ac258603cdbd4f9fd361386978d8eb7b76675f2b
Raphael Amorim <rapha850@gmail.com>
2018-08-14 20:40:59 -0300

    NOICE

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,22 +9,16 @@ function getEndpoint() {
   });
 }
 
-// Register event listener for the 'push' event.
 self.addEventListener('push', function(event) {
-  // Keep the service worker alive until the notification is created.
   event.waitUntil(
     getEndpoint()
     .then(function(endpoint) {
-      // Retrieve the textual payload from the server using a GET request.
-      // We are using the endpoint as an unique ID of the user for simplicity.
-      return fetch('./getPayload?endpoint=' + endpoint);
+     return fetch('./getPayload?endpoint=' + endpoint);
     })
     .then(function(response) {
       return response.text();
     })
     .then(function(payload) {
-      // Show a notification with title 'ServiceWorker Cookbook' and use the payload
-      // as the body.
       self.registration.showNotification('SW Example', {
         body: payload,
       });
