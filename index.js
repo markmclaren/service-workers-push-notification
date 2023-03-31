7088f0b0833420ad68a53fa025366b24c4ead89c
Mark McLaren <mark.mclaren@bristol.ac.uk>
2023-03-31 16:05:10 +0100

    Add a countdown on screen, make it 30 seconds

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 navigator.serviceWorker.register('service-worker.js');
 
-notificationOptions = {
+const notificationOptions = {
   icon: "./alarm.gif",
   requireInteraction: true,
-  vibrate: [200, 100, 200, 100, 200, 100, 200],
+  vibrate: [200, 100, 200, 100, 200, 100, 200]
 }
 
 Notification.requestPermission(function(result) {
@@ -11,7 +11,7 @@ Notification.requestPermission(function(result) {
     navigator.serviceWorker.ready.then(function(registration) {
       setTimeout(function() {
         registration.showNotification('Notification with ServiceWorker', notificationOptions);
-      }, 1 * 60 * 1000); // 1 minute in milliseconds
+      }, 30 * 1000); // 30 seconds in milliseconds
     });
   }
 });
