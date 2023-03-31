035cee8edb7d978cfd0648c39cd368aee5c01e3b
Mark McLaren <mark.mclaren@bristol.ac.uk>
2023-03-31 15:57:41 +0100

    Try notification options

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 navigator.serviceWorker.register('service-worker.js');
 
+notificationOptions = {
+  icon: "./alarm.gif",
+  requireInteraction: true,
+  vibrate: [200, 100, 200, 100, 200, 100, 200],
+}
+
 Notification.requestPermission(function(result) {
   if (result === 'granted') {
     navigator.serviceWorker.ready.then(function(registration) {
       setTimeout(function() {
-        registration.showNotification('Notification with ServiceWorker');
+        registration.showNotification('Notification with ServiceWorker', notificationOptions);
       }, 1 * 60 * 1000); // 1 minute in milliseconds
     });
   }
